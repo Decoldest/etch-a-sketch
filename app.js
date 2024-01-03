2a33d24457bf63472f792fdd0a48f861754da78f
Ryan Wong <ryanjameswong@outlook.com>
2024-01-03 12:45:39 -0500

    Add draw capability to grid pixels  upon click and drag

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 const display = document.querySelector('.pixel-container');
 
+const screenDimension = display.clientHeight;
+let ddrawingOption = 'Black';
+let gridDimension = 16;
+let isClick = false;
 
-const screenWidth = display.clientWidth;
-const screenHeight = display.clientHeight;
-let dimension = 16;
-let pixelDimension;
+display.addEventListener('mouseup', e =>{
+  if(isClick) isClick = !isClick;
+});
 
-console.log(screenHeight);
+function setPixels(screenDimension, dimension){
 
-function calcPixelDimensions(){
-  pixelDimension = Math.floor(screenHeight/dimension);
-}
-
-calcPixelDimensions();
-
-
-function setPixels(pixelDimension, dimension){
-  console.log(dimension + " " + pixelDimension);
   for (let i = dimension * dimension; i > 0 ; i--){
     const pixel = document.createElement('div');
-    pixel.style.width = pixelDimension + "px";
-    pixel.style.height = pixelDimension + "px";
-    pixel.style.outline = '1px solid black';
+    pixel.classList.add('pixel');
+    pixel.style.width = screenDimension/dimension + 'px';
+    pixel.style.height = screenDimension/dimension + 'px';
+    pixel.style.outline = '1px solid grey';
+    pixelListener();
     display.appendChild(pixel);
-
   }
 }
 
-setPixels(pixelDimension, dimension);
+function pixelListener(){
+  const drawnPixels = Array.from(document.querySelectorAll('div.pixel'));
+  for (const pixel of drawnPixels) {
+    pixel.addEventListener('mouseenter', () => {
+      if (isClick){
+        pixel.style.backgroundColor = 'black';
+      }
+    });
+    pixel.addEventListener('mousedown', e => {
+      isClick = true;
+      pixel.style.backgroundColor = 'black';
+    });
+  }
+}
+
+setPixels(screenDimension, gridDimension);
