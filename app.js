471cb519da50e8bef74cf82ec85eb6149bb5a89e
Ryan Wong <ryanjameswong@outlook.com>
2024-01-03 14:19:46 -0500

    Create erase button to allow erasing pixels

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const display = document.querySelector('.pixel-container');
+const dimensionSlider = document.getElementById('gridDimension');
 
 const screenDimension = display.clientHeight;
-let ddrawingOption = 'Black';
 let gridDimension = 16;
+let pixelColor = 'black';
 let isClick = false;
+let count = 0;
 
-display.addEventListener('mouseup', e =>{
+dimensionSlider.oninput = function() {
+  gridDimension = this.value;
+}
+
+window.addEventListener('mouseup', () =>{
   if(isClick) isClick = !isClick;
 });
 
@@ -16,25 +22,35 @@ function setPixels(screenDimension, dimension){
     pixel.classList.add('pixel');
     pixel.style.width = screenDimension/dimension + 'px';
     pixel.style.height = screenDimension/dimension + 'px';
-    pixel.style.outline = '1px solid grey';
-    pixelListener();
+    //pixel.style.outline = '1px solid gainsboro';
+    addPixelListener(pixelColor);
     display.appendChild(pixel);
   }
 }
 
-function pixelListener(){
+const eraser = document.querySelector('#eraser');
+eraser.addEventListener('click', () => {
+  pixelColor = 'white';
+  addPixelListener(pixelColor);
+});
+
+
+
+function addPixelListener(pixelColor){
   const drawnPixels = Array.from(document.querySelectorAll('div.pixel'));
   for (const pixel of drawnPixels) {
     pixel.addEventListener('mouseenter', () => {
       if (isClick){
-        pixel.style.backgroundColor = 'black';
+        pixel.style.backgroundColor = pixelColor;
+        //console.log(pixelColor);
       }
     });
-    pixel.addEventListener('mousedown', e => {
+    pixel.addEventListener('mousedown', () => {
       isClick = true;
-      pixel.style.backgroundColor = 'black';
+      pixel.style.backgroundColor = pixelColor;
     });
   }
 }
+
 
 setPixels(screenDimension, gridDimension);
